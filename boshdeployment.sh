c8f54da22600b8bc20dc3eecbff657256bf51bb5
Michael Krumpe <mkrumpe@gmail.com>
2016-11-18 16:59:06 -0500

    Initial deployment of scripts

---
+++ boshdeployment.sh
@@ -0,0 +1,102 @@
+## Prefill these values, or be prompted for to enter in temporary memory during run
+vBOSH_AWS_ACCESS_KEY_ID=
+vBOSH_AWS_SECRET_ACCESS_KEY=
+vBOSH_VPC_DOMAIN=
+vBOSH_VPC_SUBDOMAIN=
+vBOSH_AWS_REGION=us-east-1
+vBOSH_VPC_PRIMARY_AZ=us-east-1a
+vBOSH_VPC_SECONDARY_AZ=us-east-1d
+
+read -p "Have you uploaded your stub manifest file to your user's root ~/ directory? y/n"
+
+
+if [ "$vBOSH_AWS_ACCESS_KEY_ID" == ""]
+    echo "AWS ACCESS KEY
+    "
+    read vAWS_ACCESS_KEY
+    vBOSH_AWS_ACCESS_KEY_ID=$vAWS_ACCESS_KEY
+fi
+
+if [ "$vBOSH_AWS_SECRET_ACCESS_KEY" == ""]
+    echo "AWS SECRET KEY
+    "
+    read vAWS_SECRET_KEY
+    vBOSH_AWS_SECRET_ACCESS_KEY=$vAWS_SECRET_KEY
+fi
+
+if [ "$vBOSH_VPC_DOMAIN" == "" ]
+    echo "VPC Domain, pointing to CF. If you do not have one use, IP.Address.numbers.xip.io
+    "
+    read vDomain
+    vBOSH_VPC_DOMAIN=$vDomain
+fi
+
+if [ "$vBOSH_VPC_SUBDOMAIN" == "" ]
+    echo "AWS SUBDOMAIN
+    "
+    read vSUBDOMAIN
+    vBOSH_VPC_SUBDOMAIN=$vSUBDOMAIN
+fi
+
+if [ "$vBOSH_AWS_REGION" == "" ]
+    echo "AWS Region to deploy CF (Should be the same as BOSH-LITE)
+    "
+    read vREGION
+    vBOSH_AWS_REGION=vREGION
+fi
+
+if [ "$vBOSH_VPC_PRIMARY_AZ" == "" ]
+    echo "VPC Primary Domain
+    "
+    read vPRIMARY
+    vBOSH_VPC_PRIMARY_AZ=vPRIMARY
+fi
+
+if [ "$vBOSH_VPC_SECONDARY_AZ" == "" ]
+    echo "VPC Secondary Domain
+    "
+    read vSECONDARY
+    vBOSH_VPC_SECONDARY_AZ=vSECONDARY
+fi
+
+sudo apt-get update && sudo apt-get -y install git unzip
+wget https://github.com/cloudfoundry-incubator/spiff/releases/download/v1.0.7/spiff_linux_amd64.zip
+sudo unzip spiff_linux_amd64.zip -d /usr/local/bin
+
+bosh target 127.0.0.1
+
+mkdir workspace
+cd workspace
+
+git clone https://github.com/cloudfoundry/cf-release.git
+cd cf-release
+sudo ./scripts/update
+
+cd ~/workspace
+sudo gem install bundler
+
+# need manifest file uploaded to ~/
+
+cd ~/workspace/cf-release
+
+sudo  ./scripts/generate-bosh-lite-dev-manifest ~/cf-stub-aws_temp.yml
+
+cd ~/workspace
+bosh upload stemcell https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent
+
+cd ~/workspace/cf-release
+sudo bosh upload release releases/cf-246.yml
+
+export BOSH_AWS_ACCESS_KEY_ID=vBOSH_AWS_ACCESS_KEY_ID
+export BOSH_AWS_SECRET_ACCESS_KEY=vBOSH_AWS_SECRET_ACCESS_KEY
+export BOSH_VPC_DOMAIN=vBOSH_VPC_DOMAIN
+export BOSH_VPC_SUBDOMAIN=vBOSH_VPC_SUBDOMAIN
+export BOSH_AWS_REGION=vBOSH_AWS_REGION
+export BOSH_VPC_SECONDARY_AZ=vBOSH_VPC_SECONDARY_AZ
+export BOSH_VPC_PRIMARY_AZ=vBOSH_VPC_PRIMARY_AZ
+
+bosh deploy
+
+echo "Set cf api with username admin and password admin, then do cf login
+    "
+cf api --skip-ssl-validation https://$vBOSH_VPC_DOMAIN
---
+++ setupboshjump.sh
@@ -0,0 +1,119 @@
+## Prefill these values, or be prompted for to enter in temporary memory during run
+vBOSH_AWS_ACCESS_KEY_ID=
+vBOSH_AWS_SECRET_ACCESS_KEY=
+vBOSH_LITE_NAME= 
+vBOSH_LITE_SECURITY_GROUP=
+vBOSH_LITE_KEYPAIR=
+vBOSH_LITE_PRIVATE_KEY=
+vBOSH_LITE_SUBNET_ID=
+vBOSH_LITE_INSTANCE_TYPE=
+
+if [ "$vBOSH_AWS_ACCESS_KEY_ID" == ""]
+    echo "AWS ACCESS KEY
+    "
+    read vAWS_ACCESS_KEY
+    vBOSH_AWS_ACCESS_KEY_ID=$vAWS_ACCESS_KEY
+fi
+
+if [ "$vBOSH_AWS_SECRET_ACCESS_KEY" == ""]
+    echo "AWS SECRET KEY
+    "
+    read vAWS_SECRET_KEY
+    vBOSH_AWS_SECRET_ACCESS_KEY=$vAWS_SECRET_KEY
+fi
+
+if [ "$vBOSH_LITE_NAME" == ""]
+    echo "Name of the BOSH machine to deploy
+    "
+    read vBOSHNAME
+    vBOSH_LITE_NAME=$vBOSHNAME
+fi
+
+if [ "$vBOSH_LITE_SECURITY_GROUP" == ""]
+    echo "Name of the AWS Security group setup
+    "
+    read vSECURITYGROUP
+    vBOSH_LITE_SECURITY_GROUP=$vSECURITYGROUP
+fi
+
+if [ "$vBOSH_LITE_KEYPAIR" == ""]
+    echo "Name of the AWS API Key Pair to be used by Vagrant
+    "
+    read vKEYPAIR
+    vBOSH_LITE_KEYPAIR=$vKEYPAIR
+fi
+
+if [ "$vBOSH_LITE_PRIVATE_KEY" == ""]
+    echo "File path on this machine of the .pem file from your keys. Should be in ~/.ssh/ directory. If you have not uploaded, please do so now and type the full path including the ~
+    "
+    read vKEYLOC
+    vBOSH_LITE_PRIVATE_KEY=$vKEYLOC
+fi
+
+if [ "$vBOSH_LITE_SUBNET_ID" == ""]
+    echo "AWS VPC Subnet ID
+    "
+    read vSUBNET
+    vBOSH_LITE_SUBNET_ID=vSUBNET
+fi
+
+if [ "$vBOSH_LITE_INSTANCE_TYPE" == ""]
+    echo "AWS Instance type. Should be either m3.medium or m3.large
+    "
+    read vINSTANCE
+    vBOSH_LITE_INSTANCE_TYPE=$vINSTANCE
+fi
+
+#echo "update"
+apt-get update
+
+echo "install prereqs"
+sudo apt-get install build-essential ruby ruby-dev libxml2-dev libsqlite3-dev libxslt1-dev libpq-dev libmysqlclient-dev zlib1g-dev git awscli
+
+aws configure #accesskey, secretkey, region=us-east1
+
+sudo apt install linuxbrew-wrapper
+
+# ...first add the Cloud Foundry Foundation public key and package repository to your system
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
+echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+# ...then, update your local package index, then finally install the cf CLI
+sudo apt-get update
+sudo apt-get install cf-cli
+
+echo "get upstream vagrant latest"
+wget https://releases.hashicorp.com/vagrant/1.8.6/vagrant_1.8.6_x86_64.deb
+dpkg -i vagrant_1.8.6_x86_64.deb
+sudo apt-get install -f
+
+echo "gem installs"
+sudo gem install bosh_cli --no-ri --no-rdoc	##(sudo needed to install gems in /var/lib/gems) 
+
+echo "get bosh-lite"
+mkdir ~/workspace
+cd ~/workspace
+git clone https://github.com/cloudfoundry/bosh-lite
+cd bosh-lite
+
+vagrant box add cloudfoundry/bosh-lite
+
+echo "rsa keys"
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+ssh-keygen -t rsa -f "bosh_rsa" -q -N ""
+
+echo "setup vagrant"
+vagrant plugin install vagrant-aws
+vagrant plugin list
+vagrant box add dummy https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box
+
+export BOSH_AWS_ACCESS_KEY_ID=vBOSH_AWS_ACCESS_KEY_ID
+export BOSH_AWS_SECRET_ACCESS_KEY=vBOSH_AWS_SECRET_ACCESS_KEY
+export BOSH_LITE_NAME=vBOSH_LITE_NAME 
+export BOSH_LITE_SECURITY_GROUP=vBOSH_LITE_SECURITY_GROUP
+export BOSH_LITE_KEYPAIR=vBOSH_LITE_KEYPAIR
+export BOSH_LITE_PRIVATE_KEY=vBOSH_LITE_PRIVATE_KEY
+export BOSH_LITE_SUBNET_ID=vBOSH_LITE_SUBNET_ID
+export BOSH_LITE_INSTANCE_TYPE=vBOSH_LITE_INSTANCE_TYPE
+
+vagrant up
